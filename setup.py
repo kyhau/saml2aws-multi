9d402fb265ce3168e342aaa9bd24adde0fa1c4a8
kyhau <virtualda@gmail.com>
2022-12-13 11:20:27 +1100

    Test with Python 3.11, Use major version updates of flake8 and tox

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ __entry_points__ = {
 
 CLASSIFIERS = [
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: 3 :: Only",
 ]
 
@@ -40,7 +40,7 @@ setup(
     install_requires=__requirements__,
     name=__title__,
     packages=find_packages(exclude=["tests"]),
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     url=__uri__,
     version=__version__,
     zip_safe=False,
